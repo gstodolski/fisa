1f5cbf7345ece6d84cf6d7d68a07379916a7f860
Graham Stodolski <stodolski101@gmail.com>
2022-11-15 15:18:49 -0500

    Add basic gui

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-# Project goes here
+import tkinter as tk
+
+class Gui:
+    """Create a GUI window"""
+    def __init__(self):
+        self._window = tk.Tk()
+        self._window.title("FISA")
+        self._window.geometry("800x500")
+        self._window.resizable(False, False)
+
+        self._options_frame = tk.Frame(self._window)
+        self._output_frame = tk.Frame(self._window)
+
+        tk.Button(self._options_frame, text="Button 1").grid(row=1, column=1)
+
+        self._options_frame.grid(row=1, column=1)
+
+        self._window.mainloop()
+
+if __name__ == "__main__":
+    Gui()
